6f5aa73063cf1c3d7738961fa2069e14cfb58340
MoSadie <mosadie@mosadie.com>
2025-01-27 22:47:47 -0800

    Add comments

--- TemplatePlugin/TemplatePlugin.cs
+++ TemplatePlugin/TemplatePlugin.cs
@@ -29,12 +29,14 @@ namespace TemplatePlugin
                 // Configure the logger, call this directly to bypass the configuration option.
                 TemplatePlugin.Log = base.Logger;
 
+                // Version checking code, if the game version is not compatible, RegisterPlugin will return false.
                 if (!MoCore.MoCore.RegisterPlugin(this))
                 {
                     Log.LogError("Failed to register plugin with MoCore. Please check the logs for more information.");
                     return;
                 }
 
+                // Define configuration options
                 debugLogs = Config.Bind("Debug", "DebugLogs", false, "Enable additional logging for debugging");
 
                 // Here is where you would add your own initialization code
@@ -42,6 +44,15 @@ namespace TemplatePlugin
 
                 // Register the event listener, see the method OnShipLoaded for the logging code.
                 Svc.Get<Events>().AddListener<ShipLoadedEvent>(OnShipLoaded, 1);
+
+                // Other useful events to know about:
+                // - BattleStartEvent
+                // - BattleEndEvent
+                // - CampaignStartEvent
+                // - CampaignEndEvent
+                // - CrewmateCreatedEvent
+                // - CrewmateRemovedEvent
+                // - ... and more!
 
 
                 // This is an example of how to listen for the application quitting event from Unity.
@@ -55,6 +66,8 @@ namespace TemplatePlugin
             }
 
         }
+
+        // The following methods are used to log messages to the console, but ONLY when the debugLogs plugin configuration option is enabled.
 
         internal static void DebugLogInfo(string message)
         {
@@ -88,12 +101,15 @@ namespace TemplatePlugin
             }
         }
 
+        // This is the event listener for the ShipLoadedEvent we registered in the Awake method.
+
         private void OnShipLoaded(ShipLoadedEvent e)
         {
             // Since we want this log to always happen, we don't use the debugLogInfo method.
             Log.LogInfo($"Ship loaded!");
         }
 
+        // This is the event listener for the Application.quitting event.
         private void ApplicationQuitting()
         {
             // If you need to do any cleanup, do it here
@@ -103,7 +119,7 @@ namespace TemplatePlugin
 
 
 
-        // Should not need to modify anything below this line
+        // Should not need to modify anything below this line, these are used for MoCore to check game version compatibility.
 
         public string GetCompatibleGameVersion()
         {
